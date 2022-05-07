9ff0aed61bee678b850464a35a232c4b83381ad6
patrikduch <duchpatrik@icloud.com>
2022-05-07 17:19:13 +0200

    Multimodule setup

---
+++ springboot-graphql-api/src/main/java/com/patrikduch/springbootgraphqlapi/SpringbootGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlApiApplication.class, args);
+    }
+
+}
---
+++ springboot-graphql-api/src/test/java/com/patrikduch/springbootgraphqlapi/SpringbootGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
