d084f1d3806ed1e43a9794120abe014316c56a47
patrikduch <duchpatrik@icloud.com>
2022-05-07 20:13:26 +0200

    Parameterized example query

--- springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/projectdetail/HelloWorldQueryResolver.java
+++ springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/projectdetail/HelloWorldQueryResolver.java
@@ -7,4 +7,8 @@ public class HelloWorldQueryResolver implements GraphQLQueryResolver {
     public String helloWorld() {
         return "Hello World!";
     }
+
+    public String geetingMessage(String firstName, String secondName) {
+        return firstName + " "  + secondName;
+    }
 }
