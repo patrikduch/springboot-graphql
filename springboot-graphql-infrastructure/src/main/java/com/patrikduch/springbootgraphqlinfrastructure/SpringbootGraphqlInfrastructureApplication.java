4f533c4db94c5759c04523b6d5a65dacf7caadb7
patrikduch <duchpatrik@icloud.com>
2022-05-07 17:23:49 +0200

    App separation into basic modules

---
+++ springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphqlinfrastructure/SpringbootGraphqlInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootgraphqlinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlInfrastructureApplication.class, args);
+    }
+
+}
---
+++ springboot-graphql-infrastructure/src/test/java/com/patrikduch/springbootgraphqlinfrastructure/SpringbootGraphqlInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrikduch.springbootgraphqlinfrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlInfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
