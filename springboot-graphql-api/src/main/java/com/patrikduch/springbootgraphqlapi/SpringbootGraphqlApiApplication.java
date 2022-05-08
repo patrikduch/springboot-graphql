1d94ff6f5c5660196d87431f0e382d7601043924
patrikduch <duchpatrik@icloud.com>
2022-05-08 11:56:35 +0200

    JPA integration

--- springboot-graphql-api/src/main/java/com/patrikduch/springbootgraphqlapi/SpringbootGraphqlApiApplication.java
+++ springboot-graphql-api/src/main/java/com/patrikduch/springbootgraphqlapi/SpringbootGraphqlApiApplication.java
@@ -2,14 +2,24 @@ package com.patrikduch.springbootgraphqlapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// Scanning all necessary packages
+/**
+ * Entry point for Springboot GraphQL application.
+ * @author Patrik Duch
+ */
 @SpringBootApplication(scanBasePackages = {
         "com.patrikduch.springbootgraphql.core",
         "com.patrikduch.springbootgraphql.infrastructure",
         "com.patrikduch.springbootgraphql.persistence",
 })
-public class SpringbootGraphqlApiApplication {
+
+// JPA
+@EntityScan("com.patrikduch.springbootgraphql.persistence")
+@EnableJpaRepositories("com.patrikduch.springbootgraphql.persistence")
+public class SpringbootGraphqlApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGraphqlApiApplication.class, args);
