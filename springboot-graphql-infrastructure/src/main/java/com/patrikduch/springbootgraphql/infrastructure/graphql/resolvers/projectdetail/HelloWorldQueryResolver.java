9537cbae9376913a2479a8cfdfa7b8f0a295b315
patrikduch <duchpatrik@icloud.com>
2022-05-08 10:30:47 +0200

    Compilation fix

--- springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/projectdetail/HelloWorldQueryResolver.java
+++ springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/projectdetail/HelloWorldQueryResolver.java
@@ -2,7 +2,6 @@ package com.patrikduch.springbootgraphql.infrastructure.graphql.resolvers.projec
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.patrikduch.domain.dtos.MessageDto;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -13,14 +12,28 @@ import java.util.UUID;
  */
 public class HelloWorldQueryResolver implements GraphQLQueryResolver {
 
+    /**
+     * Hello world query data resolver for displaying example sequence of characters.
+     * @return Sequence of characters
+     */
     public String helloWorld() {
         return "Hello World!";
     }
 
+    /**
+     * Parameterized example GraphQL query.
+     * @param firstName First argument for passing first name
+     * @param secondName Second argument for passing surname
+     * @return Concatenation of firstName and secondName.
+     */
     public String geetingMessage(String firstName, String secondName) {
         return firstName + " "  + secondName;
     }
 
+    /**
+     * Example GraphQL query, which return POJO class.
+     * @return
+     */
     public MessageDto getCustomMessage() {
 
         var res = new MessageDto();
@@ -30,6 +43,10 @@ public class HelloWorldQueryResolver implements GraphQLQueryResolver {
         return res;
     }
 
+    /**
+     * Example GraphQL, which return list of integers.
+     * @return List of integers.
+     */
     public List<Integer> getRolDice() {
         return Arrays.asList(1,2,3,4);
     }
