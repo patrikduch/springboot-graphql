0aa9ccd7bd2f031581483bab6d5cef8e7de20d5c
patrikduch <duchpatrik@icloud.com>
2022-05-14 23:28:18 +0200

    Unused import fix

--- springboot-graphql-persistence/src/main/java/com/patrikduch/springbootgraphql/persistence/daos/ProjectDetailDaoImpl.java
+++ springboot-graphql-persistence/src/main/java/com/patrikduch/springbootgraphql/persistence/daos/ProjectDetailDaoImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import javax.validation.constraints.NotNull;
 
 @Repository
 public class ProjectDetailDaoImpl implements ProjectDetailDao {
