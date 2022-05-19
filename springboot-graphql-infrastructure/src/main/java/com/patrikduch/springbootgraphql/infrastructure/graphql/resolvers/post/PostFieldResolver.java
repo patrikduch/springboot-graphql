1958592d74e40a572fef7382d7f2889a8673c381
patrikduch <duchpatrik@icloud.com>
2022-05-20 00:05:18 +0200

    SpringBoot: Code cleanup

--- springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/post/PostFieldResolver.java
+++ springboot-graphql-infrastructure/src/main/java/com/patrikduch/springbootgraphql/infrastructure/graphql/resolvers/post/PostFieldResolver.java
@@ -2,15 +2,10 @@ package com.patrikduch.springbootgraphql.infrastructure.graphql.resolvers.post;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.patrikduch.domain.dtos.AuthorDto;
-import com.patrikduch.domain.dtos.PostDto;
-import com.patrikduch.domain.entities.AuthorEntity;
 import com.patrikduch.domain.entities.PostEntity;
 import com.patrikduch.springbootgraphql.core.interfaces.plpgsql.functions.PostFn;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * @class PostFieldResolver Data resolver for field property of class AuthorDto.
